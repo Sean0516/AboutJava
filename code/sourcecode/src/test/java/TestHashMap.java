79555b16388b2b32780ddbd23617bffa5b411fdb
Sean <zhaoxingguo@duplicall.com>
2021-09-14 17:23:34 +0800

    java 源码阅读环境构建

---
+++ code/sourcecode/src/test/java/TestHashMap.java
@@ -0,0 +1,21 @@
+import org.junit.Test;
+
+import java.util.HashMap;
+
+/**
+ * @Description TestHashMap
+ * @Author Sean
+ * @Date 2021/9/14 16:26
+ * @Version 1.0
+ */
+public class TestHashMap {
+    @Test
+    public void test(){
+        HashMap<String,String> hashMap=new HashMap<>();
+        hashMap.put("1","2");
+        hashMap.put("2","2");
+        hashMap.put("3","2");
+        hashMap.put("4","2");
+
+    }
+}
